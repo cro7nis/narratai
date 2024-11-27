87098987921b4c9733256646ef2ed319ee41a5e8
cro7nis <cro7nis@gmail.com>
2024-11-27 13:30:44 +0200

    Minor UI changes

--- src/app.py
+++ src/app.py
@@ -248,7 +248,7 @@ with gr.Blocks(
                        text_size=gr.themes.sizes.text_lg),
         css=css,
         title='Narratai', ) as app:
-    gr.Image('assets/image/Narratai_lg.png', show_label=False, show_download_button=False, show_share_button=False,
+    gr.Image('assets/image/banner.png', show_label=False, show_download_button=False, show_share_button=False,
              show_fullscreen_button=False, container=False)
     with gr.Tab('Media'):
         gr.HTML('<h2 style="text-align:center;">1. Upload media or select a sample</h1>')
@@ -327,7 +327,7 @@ with gr.Blocks(
                     then(fn=lambda: gr.update(interactive=True), inputs=None, outputs=generate_button)
 
         # file_component.clear(clear_state, inputs=state, outputs=state)
-        generate_button = gr.Button(value='Generate', variant='primary')
+        generate_button = gr.Button(value='Generate', variant='primary', icon='assets/image/logo_white.png')
         loader = gr.Label(show_label=False, visible=False)
         loader.change(change_visibility, loader, loader)
 
@@ -394,7 +394,7 @@ with gr.Blocks(
                     then(fn=lambda: gr.update(visible=True), inputs=None, outputs=output_component_2)
 
         # file_component.clear(clear_state, inputs=state, outputs=state)
-        generate_button_2 = gr.Button(value='Generate', variant='primary')
+        generate_button_2 = gr.Button(value='Generate', variant='primary', icon='assets/image/logo_white.png')
         loader_2 = gr.Label(show_label=False, visible=False)
         loader_2.change(change_visibility, loader_2, loader_2)
 
@@ -404,13 +404,13 @@ with gr.Blocks(
     with gr.Tab('About'):
         gr.Markdown("""
         ## Description
-        Narratai is an AI-powered app built for [Akashathon](https://app.buidlbox.io/akash-network/akashathon-3),
+        **NarratAI** is an AI-powered app built for [Akashathon](https://app.buidlbox.io/akash-network/akashathon-3),
         running on the decentralized Akash Network. 
         It processes audio and video files by transcribing speech into text, translating it into multiple languages, and generating AI-driven voiceovers. 
         The app uses Whisper to create accurate transcriptions with word-level timestamps, enabling precise subtitles. 
         It leverages the NLLB model to translate subtibles into many languages. 
         For voiceovers, it uses Coqui-TTS, an advanced AI technology to generate natural-sounding voices, offering options for predefined voices, voice cloning, or using the original speaker's voice. 
-        Narratai enhances media accessibility and localization while showcasing the power of decentralized infrastructure and AI. Find the code in the <a href="https://github.com/cro7nis/narratai/">Github Repo</a>
+        **NarratAI** enhances media accessibility and localization while showcasing the power of decentralized infrastructure and AI.
         
         ## Motivation
         The motivation behind building this app stemmed from my experience participating in Akash [Zealy](https://zealy.io/cw/akashnetwork/questboard) campaigns, 
@@ -423,13 +423,13 @@ with gr.Blocks(
         This inspired me to create a tool that not only supports creators but also enhances accessibility for everyone.
         
         ## AI and Tech stack
-
+        
         1. [faster-whisper](https://github.com/SYSTRAN/faster-whisper):
            - A reimplementation of OpenAI's Whisper model using CTranslate2, which is a fast inference engine for Transformer models.
            - The [medium](https://huggingface.co/Systran/faster-whisper-medium) whisper model is used because it offers a nice tradeoff of speed and accuracy.
         2. [No Language Left Behind](https://ai.meta.com/research/no-language-left-behind/):
            - Created by Meta, NLLB is a cutting-edge machine translation model designed to work with over 200 languages, including low-resource languages.
-           - Narratai uses the [nllb-200-distilled-600M](https://huggingface.co/facebook/nllb-200-distilled-600M) model.
+           - **NarratAI** uses the [nllb-200-distilled-600M](https://huggingface.co/facebook/nllb-200-distilled-600M) model.
         3. [Coqui TTS](https://github.com/coqui-ai/TTS):
            - Coqui-TTS is an open-source AI-driven text-to-speech synthesis tool capable of generating natural, human-like voices.
            - [XTTSv2](https://docs.coqui.ai/en/latest/models/xtts.html) model is used that supports 17 languages and voice cloning.
@@ -449,34 +449,36 @@ with gr.Blocks(
         gr.HTML(f'<img src="{diagram_path}" alt="Diagram">')
 
         gr.Markdown("""
-        The diagram illustrates the workflow of the **Narratai** application, which processes media files to generate accessible content with transcription, translation, and AI voiceovers.
+        The diagram illustrates the workflow of the **NarratAI** application, which processes media files to generate accessible content with transcription, translation, and AI voiceovers.
         
         1. **Input**:  
            - The user uploads a video, audio or SRT file.  
+             - Supported types: .mp4 .mkv .mov .mp3 .wav .flac .srt
+             - Current max file size: 20MB 
            - If a video is uploaded, the app extracts the audio for further processing.  
-           - If an SRT file is uploaded, the app omits the transcriptions step
+           - If an SRT file is uploaded, the app skips the transcription step entirely.
         
         2. **Transcription with Whisper**:  
            - The extracted audio is processed using the **Whisper** model to generate an accurate transcription.  
-           - **Whisper** supports multilingual transcription and provides **word-level timestamps**, making it straightforward to create an **SRT file** for subtitles.  
-           - To ensure smoother text-to-speech synthesis, **SRT segments** are refined to include full sentences or meaningful phrases.  
+           - **Whisper** supports multilingual transcription and provides **word-level timestamps**, making it straightforward to create an **SRT file** with captions.  
+           - To ensure smoother text-to-speech synthesis, **SRT segments** are refined to include full sentences or meaningful phrases.
         
         3. **Translation with NLLB**:  
            - If the user specifies a language different from the original transcription, the **NLLB** model translates the SRT file.  
            - This results in a new SRT file with segments translated into the desired language, maintaining the original timing structure.  
         
         4. **Voice Generation with XTTS**:  
-           - The **XTTS** (based on Coqui-TTS) model is applied to each SRT segment to generate audio voiceovers.  
+           - The **XTTS** model is applied to each SRT segment to generate artificial speech.  
            - Users have several options for voice generation:
              - **Predefined Speakers**: Choose from a set of available voices.  
              - **Voice Cloning**: Submit their own recordings to clone their voice.  
-             - **Input Media Voice Cloning**: Use the voice from the uploaded media file.  
+             - **Input Media Voice Cloning**: Use the voice from the uploaded media file for cloning.  
              - Adjust the generated voice speed.
         
         5. **Output Presentation**:  
            - The app combines the generated voice segments into a cohesive voiceover and presents the final output to the user. 
-           - If the input media is video then it combines it with the generated voiceover.
-           - The generated media can be downloaded
+           - If the input media is video then it combines it with the generated voiceover in a new video.
+           - The generated media can be downloaded.
            
         ## Challenges and Limitations
         Synchronizing an SRT file with AI-generated voice models poses several challenges due to differences in timing, speech dynamics, and model behavior. Here's why this process can be complex:
@@ -493,20 +495,33 @@ with gr.Blocks(
         - A simple heuristic approach is developed, to adjust the generated voice speed based on the number of words in a segment and the available time. If a segment contains many words and has limited time, the voice speed will increase. Conversely, if a segment has fewer words or more time, the voice speed will decrease.
         
         While these approaches improve the quality of the generated results, they still have limitations. For instance, voice generation for a segment must be repeated iteratively with increasing speed until it fits within the available time interval of the SRT segment. This iterative process slows down the overall voice generation, espesially for longer videos. 
+        For this reason, **NarratAI** currently supports media files up to 20MB and is not thoroughly tested on videos longer than 3 minutes.
         
-        Narratai currently supports media files up to 20MB.
+        ## Future Work
+        There are a lot of things that can be improved in the app. Would be happy to get some help with the following:
+        - Include speaker diarization and generate different voice for different speakers
+        - Improve syncronization between SRT and generated voice segments
+        - Improve UI 
+        - Support longer videos by making the voice generation process faster
         
-        Narratai is not intented for commercial purposes.
+        ## Disclaimer
+        
+        **NarratAI** is build for educational purposes and is not intented for commercial use.
         """)
 
     akash_logo = gr.Image('assets/image/akash-logo-sm.png', show_label=False, show_download_button=False,
                           show_share_button=False,
                           show_fullscreen_button=False, container=False, visible=False)
     akash_logo_path = akash_logo.value['url']
+
+    narratai_logo = gr.Image('assets/image/logo_white.png', show_label=False, show_download_button=False,
+                          show_share_button=False,
+                          show_fullscreen_button=False, container=False, visible=False)
+    narratai_logo_path = narratai_logo.value['url']
     gr.HTML(
         f'<div class="footer_main"> \
             <p> \
-                Created with ♥️ by <a href="https://x.com/cro7nis">cro7</a> and hosted on Akash Network <img src="{akash_logo_path}" alt="Akash logo" width="20" style="vertical-align: top; display: inline;"> \
+                <img src="{narratai_logo_path}" alt="Akash logo" width="20" style="vertical-align: top; display: inline;"> created with ♥️ by <a href="https://x.com/cro7nis">cro7</a> and hosted on Akash Network <img src="{akash_logo_path}" alt="Akash logo" width="20" style="vertical-align: top; display: inline;"> \
             </p> \
         </div>\
         ') \
